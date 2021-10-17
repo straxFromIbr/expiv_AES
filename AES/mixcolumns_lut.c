67cb29e35c45f8ac145d52d55360f6eea9e21641
straxFromIbr <haga.yuya2.2016@gmail.com>
2021-10-17 22:40:16 +0900

    fix: mixcolumn_lut

--- AES/mixcolumns_lut.c
+++ AES/mixcolumns_lut.c
@@ -5,6 +5,7 @@ extern unsigned char mbox02[256];
 extern unsigned char mbox03[256];
 
 void MixColumns(unsigned char state[4 * Nb]) {
+
   unsigned char res[4 * Nb];
   for (int i = 0; i <= 12; i += 4) {
     res[i] =
@@ -15,6 +16,10 @@ void MixColumns(unsigned char state[4 * Nb]) {
         state[i] ^ state[i + 1] ^ mbox02[state[i + 2]] ^ mbox03[state[i + 3]];
     res[i + 3] =
         mbox03[state[i]] ^ state[i + 1] ^ state[i + 2] ^ mbox02[state[i + 3]];
+
+    state[i] = res[i];
+    state[i+1] = res[i+1];
+    state[i+2] = res[i+2];
+    state[i+3] = res[i+3];
   }
-  state = res;
 }
